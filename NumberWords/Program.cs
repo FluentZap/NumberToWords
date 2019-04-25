42871c6db99de47da4e684b2a2e7a7a7d705dd7d
Student <example@example.com>
2019-04-25 13:46:36 -0700

    add basic file structure and BDD

---
+++ NumberWords/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumberWords
+{
+    class RockPaperScissors
+    {
+        public enum Listof_Choices
+        {
+            rock,
+            paper,
+            scissors
+        }
+
+        public enum Listof_Results 
+        {
+            tie,
+            player1,
+            player2,
+            badChoices
+        }
+
+        public void RPS()
+        {
+            
+        }
+
+
+    }
+}
